6ae3c9688f53147a065a7b2c956c4284df2a3aad
blanche <bcohen@nyx.net>
2020-11-11 20:05:34 -0700

    First Python program

---
+++ p1.py
@@ -0,0 +1,9 @@
+# add up all the numbers between 1 and 100
+sum = 0
+# range does not include the ending value, so
+# this is really adding up all the number from
+# 1 to 99
+for i in range (1,100):
+    sum = sum + i
+print ("answer is ", sum)
+
