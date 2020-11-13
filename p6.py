649c06c98cfc9112130a78216e561d1058e4d332
blanchecohen <blanchecohen@users.noreply.github.com>
2020-11-12 19:20:16 -0700

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,19 @@
+import math
+
+myList = [4,3,7,8,9,'fred', math.sqrt(9),42]
+print myList
+print myList[4]
+print myList[6]
+myList[2] = 'sam'
+print myList
+theList = list()
+print theList
+theList.insert(5, 92)
+print theList[0]
+theList.insert(1, 99)
+print theList[1]
+print len(theList)
+print theList
+print myList.sort()
+print myList
+
