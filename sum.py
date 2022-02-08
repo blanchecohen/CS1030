db6dc83fd803dff6b51dd99fd5ab47ba643276d2
blanchecohen <blanchecohen@users.noreply.github.com>
2022-02-07 17:34:46 -0700

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,13 @@
+# problem: add up all the numbers from 1 to 100
+# put the result into "sum"
+# print the value sum
+#
+#sum = 1 + 2+ 3 + 4 + 5 + 6 + 7.....
+#
+i = 1
+sum = 0
+while i <= 100:
+   
+   
+   
+print("the answer is ", sum)
